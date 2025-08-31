df89fd4fd4570d7f2b0ab898447b8679c5352988
rvwierzba <rafaelwierzba@gmail.com>
2025-08-31 10:47:31 -0300

    Solução definitiva para os Dockerfiles de deploy

--- Lead2Buy.API/entrypoint.sh
+++ Lead2Buy.API/entrypoint.sh
@@ -5,7 +5,8 @@ echo "Aguardando o banco de dados ficar pronto... (espera de 20s)"
 sleep 20
 
 echo "Executando migrações do banco de dados..."
-dotnet ef database update
+# A CORREÇÃO FINAL: Usa o caminho completo para a ferramenta 'ef'
+/root/.dotnet/tools/dotnet-ef database update
 
 echo "Migrações concluídas. Iniciando a aplicação..."
 exec dotnet Lead2Buy.API.dll
