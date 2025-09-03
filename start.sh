8e64abaa0896fe939012c5d0ba7b1d3b5633f256
Rafael Vinicius Wierzba <rafaelwierzba@gmail.com>
2025-09-03 18:55:07 -0300

    VAIIIIIIIIIIIII

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Inicia o servidor Ollama em background
-/usr/local/bin/ollama serve &
-
-# Aguarda 10 segundos para dar tempo ao Ollama de iniciar
+echo "Iniciando o servidor Ollama em background..."
+ollama serve &
 sleep 10
 
 echo "Tentando baixar o modelo phi4-mini em background..."
-/usr/local/bin/ollama pull phi4-mini:q4_0 &
+ollama pull phi4-mini:q4_0 &
 
 echo "Iniciando a API .NET..."
-# Inicia a sua API .NET, que fica como o processo principal do contêiner
 dotnet Lead2Buy.API.dll
