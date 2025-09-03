8572813a01c310b22d64e480ca4719dbc1d82693
Rafael Vinicius Wierzba <rafaelwierzba@gmail.com>
2025-09-02 21:31:11 -0300

    Atualizando o appSettings com a connStr correta

--- start.sh
+++ start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-/usr/local/bin/ollama serve &
+echo "Iniciando o servidor Ollama em background..."
+# Chama 'ollama' diretamente, sem o caminho completo
+ollama serve &
+
+# Aguarda 10 segundos para dar tempo ao Ollama de iniciar
 sleep 10
+
 echo "Tentando baixar o modelo phi4-mini em background..."
-/usr/local/bin/ollama pull phi4-mini:q4_0 &
+ollama pull phi4-mini:q4_0 &
+
 echo "Iniciando a API .NET..."
 dotnet Lead2Buy.API.dll
