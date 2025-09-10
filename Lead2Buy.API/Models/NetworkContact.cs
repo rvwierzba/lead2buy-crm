03ac37dfc741611c5a7634f42462bb982110ba58
Rafael Vinicius Wierzba <rafaelwierzba@gmail.com>
2025-09-10 09:42:24 -0300

    fix(api): Remove 'required' keyword from NetworkContact model to fix EF Core conflict

--- Lead2Buy.API/Models/NetworkContact.cs
+++ Lead2Buy.API/Models/NetworkContact.cs
@@ -10,7 +10,7 @@ namespace Lead2Buy.API.Models
 
         [Required]
         [MaxLength(100)]
-        public required string Name { get; set; }
+        public string Name { get; set; } 
 
         [MaxLength(20)]
         public string? PhoneNumber { get; set; }
@@ -18,30 +18,27 @@ namespace Lead2Buy.API.Models
         [MaxLength(150)]
         public string? Email { get; set; }
 
-        // --- NOVOS CAMPOS AQUI ---
         [Url]
         public string? FacebookUrl { get; set; }
 
-        // Armazenaremos como string para incluir o código do país, ex: +5511999999999
         [Phone]
         [MaxLength(20)]
         public string? WhatsAppNumber { get; set; }
 
         [Url]
         public string? YouTubeChannelUrl { get; set; }
-        
+
         [Url]
         public string? LinkedInUrl { get; set; }
 
         [Url]
         public string? InstagramUrl { get; set; }
-        
+
         [Url]
         public string? OtherSocialUrl { get; set; }
 
         public string? Notes { get; set; }
 
-        // Relacionamento com o Usuário
         public int UserId { get; set; }
         [ForeignKey("UserId")]
         public virtual User? User { get; set; }
